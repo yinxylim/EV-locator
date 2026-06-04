95e34d744f1bdbac1924f01e19965118e841e1b3
yinxylim <yinxylim@gmail.com>
2026-06-04 22:59:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,33 @@ import streamlit as st
 import requests
 import pandas as pd
 
-# The API URL for Malaysia (Country Code 'MY')
-API_URL = "https://api.openchargemap.io/v3/poi/?output=json&countrycode=MY&maxresults=500&compact=true&verbose=false"
-
-@st.cache_data(ttl=3600) # Cache for 1 hour to stay under API limits
+@st.cache_data(ttl=3600)
 def get_ev_chargers():
-    response = requests.get(API_URL)
-    data = response.json()
+    try:
+        response = requests.get(API_URL)
+        
+        # Check if the request was successful (status code 200)
+        response.raise_for_status() 
+        
+        # Try to parse the JSON
+        data = response.json()
+        
+        # Process the data
+        chargers = []
+        for item in data:
+            chargers.append({
+                'Station': item.get('AddressInfo', {}).get('Title'),
+                'lat': item.get('AddressInfo', {}).get('Latitude'),
+                'lon': item.get('AddressInfo', {}).get('Longitude'),
+                'Address': item.get('AddressInfo', {}).get('AddressLine1')
+            })
+        return pd.DataFrame(chargers)
+        
+    except requests.exceptions.HTTPError as http_err:
+        st.error(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.JSONDecodeError:
+        st.error("The server did not return valid JSON. Check if the URL is correct or if the API is down.")
+    except Exception as err:
+        st.error(f"An error occurred: {err}")
     
-    # Process the data into a format we can use
-    chargers = []
-    for item in data:
-        chargers.append({
-            'Station': item.get('AddressInfo', {}).get('Title'),
-            'lat': item.get('AddressInfo', {}).get('Latitude'),
-            'lon': item.get('AddressInfo', {}).get('Longitude'),
-            'Address': item.get('AddressInfo', {}).get('AddressLine1')
-        })
-    return pd.DataFrame(chargers)
-
-df = get_ev_chargers()
-st.write(df) # This will display the list automatically!
+    return pd.DataFrame() # Return empty dataframe on error
